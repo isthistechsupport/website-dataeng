839c0c21c33aebcaeee2612f62e44147b037151e
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-10-24 14:23:14 -0500

    updated image from ext package to core

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import i18n from "astro-i18n";
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [i18n(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [
+    i18n()
+  ]
 });
